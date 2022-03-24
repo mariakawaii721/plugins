dbd4df9722f700bb3a781fbcc75ee4e64a163858
mariakawaii721 <101947084+mariakawaii721@users.noreply.github.com>
2022-03-24 19:31:41 +0100

    Add files via upload

--- adminCommands/src/main/java/me/mariakawaii/comandosadmin/events/banEvent.java
+++ adminCommands/src/main/java/me/mariakawaii/comandosadmin/events/banEvent.java
@@ -1,6 +1,7 @@
 package me.mariakawaii.comandosadmin.events;
 
 import me.mariakawaii.comandosadmin.classes.banGUI;
+import me.mariakawaii.comandosadmin.classes.customInventory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -22,38 +23,37 @@ public class banEvent implements Listener {
 
     @EventHandler
     public void onClickMenu(InventoryClickEvent e){
-
         Player p = (Player) e.getWhoClicked();
-        Inventory banJugadorGUI = Bukkit.createInventory(p,9,"Confirmar ban");
+        Inventory banJugadorGUI = Bukkit.createInventory(p,9,ChatColor.BLACK + "Confirmar ban");
         ItemStack is = e.getCurrentItem();
         if(is.getType().equals(Material.PLAYER_HEAD) && e.getCurrentItem().getType() == Material.PLAYER_HEAD){
-            p.sendMessage("asdasddasdasdasdasd");
             Player aBanear = p.getServer().getPlayer(e.getCurrentItem().getItemMeta().getDisplayName());
-            p.openInventory(prueba(p,aBanear));
+            p.openInventory(confirmarBAN(p,aBanear));
         }
+        e.setCancelled(true);
     }
 
-    public Inventory prueba(Player p, Player aBanear){
-        Inventory asd = Bukkit.createInventory(p,9,"Confirmar ban");
+    public Inventory confirmarBAN(Player p, Player aBanear){
+        Inventory inventarioBAN = Bukkit.createInventory(p,9,"Confirmar ban");
 
         ItemStack ban = new ItemStack(Material.IRON_AXE,1);
         ItemMeta banMeta = ban.getItemMeta();
-        banMeta.setDisplayName(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Banear");
+        banMeta.setDisplayName(ChatColor.DARK_RED + "Banear");
         ban.setItemMeta(banMeta);
-        asd.setItem(0,ban);
+        inventarioBAN.setItem(0,ban);
 
         ItemStack cabeza = new ItemStack(Material.PLAYER_HEAD,1);
         ItemMeta cabezaMeta = cabeza.getItemMeta();
-        cabezaMeta.setDisplayName(aBanear.getDisplayName());
+        cabezaMeta.setDisplayName(ChatColor.AQUA + aBanear.getDisplayName());
         cabeza.setItemMeta(cabezaMeta);
-        asd.setItem(4,cabeza);
+        inventarioBAN.setItem(4,cabeza);
 
         ItemStack volver = new ItemStack(Material.BARRIER,1);
         ItemMeta volverMeta = volver.getItemMeta();
         volverMeta.setDisplayName(ChatColor.RED + "Volver");
         volver.setItemMeta(volverMeta);
-        asd.setItem(8,volver);
-        return asd;
+        inventarioBAN.setItem(8,volver);
+        return inventarioBAN;
     }
 
 
